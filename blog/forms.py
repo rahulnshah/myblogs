9f1fff535a014ff90a8eb5620d4581972f3c52ff
Rahul Shah <rahulnshah3012@gmail.com>
2022-05-09 22:18:32 -0400

    made a code box to input code into my posts

--- blog/forms.py
+++ blog/forms.py
@@ -6,8 +6,8 @@ class PostForm(forms.ModelForm):
 
     class Meta:  # helper class 
         model = Post
-        fields = ['title', 'text'] #  once again we will create a link to the forms page, a URL, a view and a template
+        fields = ['title', 'text', 'codeText'] #  once again we will create a link to the forms page, a URL, a view and a template
 
 # this will be my search box 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    your_name = forms.CharField(label='Enter keywords', max_length=100)
