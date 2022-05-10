e381ec89996ceb292be1e652f7a0e1dacdd4bf70
Rahul Shah <rahulnshah3012@gmail.com>
2022-05-10 14:35:07 -0400

    alomst done fixing search box

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,17 @@ from .forms import NameForm
 
 # Create your views here.
 def post_list(request):
+    # Note: Control blocks like If statements in Python do not count and the variables used or initialized inside the block of an If statement can also be used and accessed outside its scope. 
     posts = Post.objects.all() # returns a queryset 
-    post_Query_Set = posts.filter(published_date__lte=timezone.now()).order_by('published_date') # also a query set 
-    return render(request, 'blog/post_list.html', {'posts' : post_Query_Set})
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            #fetch the search results
+            posts = Post.objects.all().filter(text__contains=form.cleaned_data['your_name']) # returns a queryset 
+            return render(request, 'blog/post_list.html', {'posts' : posts, "form" : form})
+    else:
+        form = NameForm()
+    return render(request, 'blog/post_list.html', {'posts' : posts, "form" : form})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk) # The following example gets the object with the primary key pk from Post
@@ -47,17 +55,3 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
     
-
-def search_post(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:, blank field is not allowed by deafualt in Django, so same behavior will apply here as in methods above
-        if form.is_valid():
-            posts = Post.objects.all().filter(text__contains=form.cleaned_data['your_name']) # returns a queryset 
-            return render(request, 'blog/search_results.html', {'posts' : posts})
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-    return render(request, 'blog/name.html', {'form': form})
